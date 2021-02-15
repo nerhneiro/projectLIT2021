45076c064f34b8ea9bbf2e909344176c462f942f
Irene <nerhneiro@gmail.com>
2021-02-15 21:20:41 +0300

    continue trying to add auth

--- projectLIT/projectLIT/settings.py
+++ projectLIT/projectLIT/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'authapp.SiteUser'
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
