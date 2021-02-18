78518f9ac2e38663c2ad40de1d6e09dd20758fc7
Irene <nerhneiro@gmail.com>
2021-02-18 21:17:39 +0300

    Revert "try to add albums to user doesnt work"
    This reverts commit 2060b1831adf9b0bf678dfe57a722f512c7fb798.

--- projectLIT/authapp/models.py
+++ projectLIT/authapp/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.postgres.fields import ArrayField
-from ..mainapp.models import Album, Artist, Label, Tag
+
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
     age = models.PositiveIntegerField(verbose_name = 'возраст', default=18)
-    albums = ArrayField(models.ForeignKey(Album, verbose_name='album', blank=True), verbose_name='albums')
