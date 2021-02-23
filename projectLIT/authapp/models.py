4afcbc2c277d24f9d7b035b88f280b4c33da8413
Irene <nerhneiro@gmail.com>
2021-02-23 13:10:22 +0300

    add edit page
    + add country bull and blank
    + add css to edit page

--- projectLIT/authapp/models.py
+++ projectLIT/authapp/models.py
@@ -8,4 +8,5 @@ class SiteUser(AbstractUser):
     age = models.PositiveIntegerField(verbose_name='age', null=True, blank=True)
     #albums = ArrayField(models.ForeignKey(Album, verbose_name='album', blank=True, on_delete=models.CASCADE), verbose_name='albums')
     albums = models.ManyToManyField(Album, verbose_name='albums', blank=True, related_name='album_users')
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, verbose_name='email')
+    country = models.CharField(max_length=128, blank=True, null=True, verbose_name='country')
